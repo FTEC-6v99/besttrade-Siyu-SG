e1ebbdaa5c6c13246f3b007f365d46a1f79446f4
Siyu-SG <gongsiyu040627@gmail.com>
2021-10-31 22:46:58 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 dbparams = {
-    'host': '',
-    'username': '',
-    'password': '',
-    'database': ''
+    'host': 'database-2.cpoz9e9k5guh.us-east-1.rds.amazonaws.com',   #endpoint from AWS
+    'username': 'admin',
+    'password': 'besttradedatabase',
+    'database': 'WebDevProject'
 }
