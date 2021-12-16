db3142c91f51e80b332168835858d76ddaa189e3
Siyu-SG <gongsiyu040627@gmail.com>
2021-12-16 08:07:14 -0600

    Update app.py

--- app/src/app.py
+++ app/src/app.py
@@ -2,16 +2,17 @@ from flask import Flask
 from app.src.api.blueprints.investorbp import investorbp
 from app.src.api.blueprints.accountbp import accountbp
 from app.src.api.blueprints.portfoliobp import portfoliobp
-
+from app.src.api.blueprints.uibp import uibp
 
 app = Flask(__name__)
 #register all endpoints
 app.register_blueprint(investorbp)
 app.register_blueprint(accountbp)
 app.register_blueprint(portfoliobp)
+app.register_blueprint(uibp)
 
 
-
+# Test 
 # @app.route('/')
 # def default():
 #     return  "Hello world!"
