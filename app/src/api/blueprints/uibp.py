d4fb254953ebde2986e2213e39ecbc5d84851bfd
Siyu-SG <31021926+Siyu-SG@users.noreply.github.com>
2021-12-16 08:02:17 -0600

    Project_AddingUI

---
+++ app/src/api/blueprints/uibp.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+from app.src.domain.Investor import Investor
+import app.src.db.dao as dao
+
+uibp = Blueprint('ui',__name__, url_prefix = '/ui')
+
+@uibp.route('/', methods = ['GET'])
+def main():
+    return render_template('home.html') 
+
+@uibp.route('/about', methods = ['GET'])
+def about():
+    return render_template('about.html') 
+
+@uibp.route('/investors', methods = ['GET'])
+def investor_all():
+    investors = dao.get_all_investor()
+    return render_template('investors.html', investors = investors)  
